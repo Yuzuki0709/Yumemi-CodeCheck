d823f65e2c42e7022ef8c6c2b683c153c3b8822b
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-15 07:22:37 +0900

    fix: RepositorySearchViewControllerにfinalを付与

--- iOSEngineerCodeCheck/RepositorySearchViewController.swift
+++ iOSEngineerCodeCheck/RepositorySearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RepositorySearchViewController: UITableViewController, UISearchBarDelegate {
+final class RepositorySearchViewController: UITableViewController, UISearchBarDelegate {
     
     @IBOutlet private weak var searchBar: UISearchBar!
     
