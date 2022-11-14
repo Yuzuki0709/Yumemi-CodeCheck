22bcbf4bffde36730a198e16711bb265f3646856
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-15 07:38:33 +0900

    feat: RepositoryDetailViewControllerにsetLabels関数を追加

--- iOSEngineerCodeCheck/RepositoryDetailViewController.swift
+++ iOSEngineerCodeCheck/RepositoryDetailViewController.swift
@@ -23,13 +23,17 @@ final class RepositoryDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = repository["full_name"] as? String
-        languageLabel.text = "Written in \(repository["language"] as? String ?? "")"
-        starsLabel.text = "\(repository["stargazers_count"] as? Int ?? 0) stars"
-        watchersLabel.text = "\(repository["wachers_count"] as? Int ?? 0) watchers"
-        forksLabel.text = "\(repository["forks_count"] as? Int ?? 0) forks"
-        issuesLabel.text = "\(repository["open_issues_count"] as? Int ?? 0) open issues"
+        setLabels()
         fetchUserImage()
+    }
+    
+    private func setLabels() {
+        titleLabel.text    = repository["full_name"] as? String
+        languageLabel.text = "Written in \(repository["language"] as? String ?? "")"
+        starsLabel.text    = "\(repository["stargazers_count"] as? Int ?? 0) stars"
+        watchersLabel.text = "\(repository["wachers_count"] as? Int ?? 0) watchers"
+        forksLabel.text    = "\(repository["forks_count"] as? Int ?? 0) forks"
+        issuesLabel.text   = "\(repository["open_issues_count"] as? Int ?? 0) open issues"
     }
     
     private func fetchUserImage(){
