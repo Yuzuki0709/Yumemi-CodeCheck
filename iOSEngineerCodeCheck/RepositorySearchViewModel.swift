beb5b6252e8de23c9fc568d002fad04298ce6459
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-15 17:59:44 +0900

    feat: RepositorySearchViewModelにViewModelTypeを準拠させた

--- iOSEngineerCodeCheck/RepositorySearchViewModel.swift
+++ iOSEngineerCodeCheck/RepositorySearchViewModel.swift
@@ -10,3 +10,29 @@ final class RepositorySearchViewModel {
         self.githubAPI = githubAPI
     }
 }
+
+extension RepositorySearchViewModel: ViewModelType {
+    struct Input {
+        let searchText:         Observable<String>
+        let searchButtonTapped: Observable<Void>
+        let selectedRepository: Observable<IndexPath>
+    }
+    
+    struct Output {
+        let repositories:       Driver<[GitHubRepository]>
+        let selectedRepository: Driver<GitHubRepository>
+        let searchDescription:  Driver<String>
+    }
+    
+    func transform(input: Input) -> Output {
+        let repositories       = BehaviorRelay<[GitHubRepository]>(value: [])
+        let selectedRepository = PublishRelay<GitHubRepository>()
+        let searchDescription  = PublishRelay<String>()
+        
+        return Output(
+            repositories: repositories.asDriver(),
+            selectedRepository: selectedRepository.asDriver(onErrorDriveWith: .empty()),
+            searchDescription: searchDescription.asDriver(onErrorDriveWith: .empty())
+        )
+    }
+}
