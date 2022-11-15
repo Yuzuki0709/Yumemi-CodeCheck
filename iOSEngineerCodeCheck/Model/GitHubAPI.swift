efa3b73e1ce85522c8e3a8d138615e1d32593a50
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-15 15:37:09 +0900

    feat: GitHubAPIをGitHubAPIProtocolに準拠するようにした

--- iOSEngineerCodeCheck/Model/GitHubAPI.swift
+++ iOSEngineerCodeCheck/Model/GitHubAPI.swift
@@ -18,3 +18,22 @@ final class GitHubAPI {
         self.provider = provider
     }
 }
+
+extension GitHubAPI: GitHubAPIProtocol {
+    func searchRepositories(keyword: String, completion: @escaping ((Result<[GitHubRepository], Error>) -> Void)) {
+        self.provider.request(.repository(keyword: keyword)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let searchResult = try JSONDecoder().decode(SearchResponse<GitHubRepository>.self, from: response.data)
+                    completion(.success(searchResult.items))
+                } catch {
+                    completion(.failure(error))
+                }
+                
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
