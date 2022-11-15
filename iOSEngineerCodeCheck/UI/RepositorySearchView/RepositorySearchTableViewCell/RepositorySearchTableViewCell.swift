3846185e84a5f33dc7ab8233eb92f5d0a5c4a2ed
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-15 20:22:24 +0900

    delete: いらないコードの削除

--- iOSEngineerCodeCheck/UI/RepositorySearchView/RepositorySearchTableViewCell/RepositorySearchTableViewCell.swift
+++ iOSEngineerCodeCheck/UI/RepositorySearchView/RepositorySearchTableViewCell/RepositorySearchTableViewCell.swift
@@ -37,16 +37,4 @@ class RepositorySearchTableViewCell: UITableViewCell {
             languageIcon.isHidden = true
         }
     }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
