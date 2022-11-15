049778d19de7c71d33fc7d6b348e2902d6ba3985
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-15 19:07:14 +0900

    add: RepositorySearchTableViewCell.swiftと.xibを追加

---
+++ iOSEngineerCodeCheck/UI/RepositorySearchView/RepositorySearchTableViewCell/RepositorySearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepositorySearchTableViewCell.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 岩本竜斗 on 2022/11/15.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class RepositorySearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
