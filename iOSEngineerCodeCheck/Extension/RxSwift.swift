a14653dc311b9bc0d06feec0a6a83c3f5dc7af36
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-15 21:11:45 +0900

    feat: RxSwiftにerrors関数を追加

--- iOSEngineerCodeCheck/Extension/RxSwift.swift
+++ iOSEngineerCodeCheck/Extension/RxSwift.swift
@@ -4,4 +4,8 @@ extension RxSwift.ObservableType where Element: RxSwift.EventConvertible {
     public func elements() -> RxSwift.Observable<Element.Element> {
         return compactMap { $0.event.element }
     }
+    
+    public func errors() -> RxSwift.Observable<Swift.Error> {
+        return compactMap { $0.event.error }
+    }
 }
