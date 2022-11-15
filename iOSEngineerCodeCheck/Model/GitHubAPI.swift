744d89f863404d7f74d883f0cec7e69ccc923091
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-15 15:31:04 +0900

    add: GitHubAPI.swiftを追加

---
+++ iOSEngineerCodeCheck/Model/GitHubAPI.swift
@@ -0,0 +1,19 @@
+import Foundation
+import Moya
+
+protocol GitHubAPIProtocol {
+    
+    /// APIからレポジトリを取得する関数
+    /// - Parameters:
+    ///   - keyword: 検索するキーワード
+    ///   - completion: 取得後の処理
+    func searchRepositories(keyword: String, completion: @escaping (([GitHubRepository]) -> Void))
+}
+
+final class GitHubAPI {
+    private let provider: MoyaProvider<GitHubAPITarget>
+    
+    init(provider: MoyaProvider<GitHubAPITarget> = MoyaProvider<GitHubAPITarget>()) {
+        self.provider = provider
+    }
+}
