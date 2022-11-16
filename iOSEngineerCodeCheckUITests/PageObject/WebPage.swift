01d0d0e9cb1646a7f7aeb0daa570d1a4ea98c61f
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-16 21:46:25 +0900

    feat: WebPageにIDsを追加

--- iOSEngineerCodeCheckUITests/PageObject/WebPage.swift
+++ iOSEngineerCodeCheckUITests/PageObject/WebPage.swift
@@ -2,6 +2,10 @@ import XCTest
 
 final class WebPage: PageObject {
     
+    private enum IDs {
+        static let webView = "web_showweb_webview"
+    }
+    
     private let app: XCUIApplication
     
     required init(application: XCUIApplication) {
