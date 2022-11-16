70eb8768fd81ccf8cd159872dd2f9855482d3ad6
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-16 12:45:06 +0900

    feat: transform内にホームページが存在するかどうかを判別する処理を追加

--- iOSEngineerCodeCheck/UI/RepositoryDetailView/RepositoryDetailViewModel.swift
+++ iOSEngineerCodeCheck/UI/RepositoryDetailView/RepositoryDetailViewModel.swift
@@ -24,8 +24,24 @@ extension RepositoryDetailViewModel: ViewModelType {
     }
     
     func transform(input: Input) -> Output {
+        
+        var homepageURL: URL? = nil
+        
         let isExistHomepage = BehaviorRelay<Bool>(value: false)
         let goHomepageView  = PublishRelay<URL>()
+        
+        input.viewWillAppear
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                
+                // ホームページが存在するかどうかを判別
+                if let homepageURLString = self.repository.homepage,
+                   !homepageURLString.isEmpty {
+                    homepageURL = URL(string: homepageURLString)
+                    isExistHomepage.accept(true)
+                }
+            })
+            .disposed(by: disposeBag)
         
         return Output(
             isExistHomepage: isExistHomepage.asDriver(),
