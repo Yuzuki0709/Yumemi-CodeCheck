a1da066ba225e13ba6b1a6d0f19371bbe904e2be
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-17 08:07:23 +0900

    feat: RepositoryDetailViewModelTestにsetUp関数を追加

--- iOSEngineerCodeCheckTests/ViewModel/RepositoryDetailViewModelTest.swift
+++ iOSEngineerCodeCheckTests/ViewModel/RepositoryDetailViewModelTest.swift
@@ -13,4 +13,17 @@ final class RepositoryDetailViewModelTest: XCTestCase {
     var viewModel:       RepositoryDetailViewModel!
     var input:           RepositoryDetailViewModel.Input!
     var output:          RepositoryDetailViewModel.Output!
+    
+    override func setUp() {
+        scheduler  = TestScheduler(initialClock: 0)
+        disposeBag = DisposeBag()
+        
+        viewWillAppear  = PublishRelay<Void>()
+        tableCellTapped = PublishRelay<IndexPath>()
+        
+        input = RepositoryDetailViewModel.Input(
+            viewWillAppear: viewWillAppear.asObservable(),
+            tableCellTapped: tableCellTapped.asObservable()
+        )
+    }
 }
