5b3e63a374798904ba2149c1abb0f7c40a3d82ac
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-16 16:40:24 +0900

    feat: WebViewModelにViewModelTypeを準拠させた

--- iOSEngineerCodeCheck/UI/WebView/WebViewModel.swift
+++ iOSEngineerCodeCheck/UI/WebView/WebViewModel.swift
@@ -6,3 +6,29 @@ import RxCocoa
 final class WebViewModel {
     private let disposeBag = DisposeBag()
 }
+
+extension WebViewModel: ViewModelType {
+    struct Input {
+        let didStartLoad:  Observable<WKNavigation>
+        let didFinishLoad: Observable<WKNavigation>
+        let didFailLoad:   Observable<(WKNavigation, Error)>
+    }
+    
+    struct Output {
+        let loading:    Driver<Void>
+        let loadFinish: Driver<Void>
+        let loadFail:   Driver<WebLoadError>
+    }
+    
+    func transform(input: Input) -> Output {
+        let loading    = PublishRelay<Void>()
+        let loadFinish = PublishRelay<Void>()
+        let loadFail   = PublishRelay<WebLoadError>()
+        
+        return Output(
+            loading: loading.asDriver(onErrorDriveWith: .empty()),
+            loadFinish: loadFinish.asDriver(onErrorDriveWith: .empty()),
+            loadFail: loadFail.asDriver(onErrorDriveWith: .empty())
+        )
+    }
+}
