0d91bedc5cba6e7b64f019fbc47256fb19ec1ca7
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-16 12:38:38 +0900

    feat: RepositoryDetailViewModelにViewModelTypeを準拠させた

--- iOSEngineerCodeCheck/UI/RepositoryDetailView/RepositoryDetailViewModel.swift
+++ iOSEngineerCodeCheck/UI/RepositoryDetailView/RepositoryDetailViewModel.swift
@@ -11,3 +11,25 @@ final class RepositoryDetailViewModel {
         self.repository = repository
     }
 }
+
+extension RepositoryDetailViewModel: ViewModelType {
+    struct Input {
+        let viewWillAppear:  Observable<Void>
+        let tableCellTapped: Observable<IndexPath>
+    }
+    
+    struct Output {
+        let isExistHomepage: Driver<Bool>
+        let goHomepageView:  Driver<URL>
+    }
+    
+    func transform(input: Input) -> Output {
+        let isExistHomepage = BehaviorRelay<Bool>(value: false)
+        let goHomepageView  = PublishRelay<URL>()
+        
+        return Output(
+            isExistHomepage: isExistHomepage.asDriver(),
+            goHomepageView: goHomepageView.asDriver(onErrorDriveWith: .empty())
+        )
+    }
+}
