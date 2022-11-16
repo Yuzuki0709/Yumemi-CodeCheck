ccf2775a4bd88ce24782dfeb0d2e68524485edfb
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-16 20:19:19 +0900

    feat: RepositoryDetailPageにIDsを追加

--- iOSEngineerCodeCheckUITests/PageObject/RepositoryDetailPage.swift
+++ iOSEngineerCodeCheckUITests/PageObject/RepositoryDetailPage.swift
@@ -2,6 +2,12 @@ import XCTest
 
 final class RepositoryDetailPage: PageObject {
     
+    private enum IDs {
+        static let detailTableView = "detail_showdetail_tableview"
+        static let homepageCell    = "detail_homepage_cell"
+        static let readmeCell      = "detail_readme_cell"
+    }
+    
     private let app: XCUIApplication
     
     required init(application: XCUIApplication) {
