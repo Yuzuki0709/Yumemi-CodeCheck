1731fe802d9b635c3e6d0a8e753e83d1cf43a9d2
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-16 10:33:27 +0900

    feat: WebViewControllerにmake関数を追加

--- iOSEngineerCodeCheck/UI/WebView/WebViewController.swift
+++ iOSEngineerCodeCheck/UI/WebView/WebViewController.swift
@@ -2,6 +2,13 @@ import UIKit
 import WebKit
 
 final class WebViewController: UIViewController {
+    
+    static func make(url: URL) -> WebViewController {
+        let view = WebViewController.instantiate(storyboardName: "WebView")
+        view.url = url
+        return view
+    }
+    
     private var url: URL!
 }
 
