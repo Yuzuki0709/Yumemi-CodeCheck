2f377c3d19da382320c7084ace894c7c111404a4
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-16 20:36:13 +0900

    feat: RepositoryDetailViewUITestにtestShowCells関数を追加

--- iOSEngineerCodeCheckUITests/RepositoryDetailViewUITest.swift
+++ iOSEngineerCodeCheckUITests/RepositoryDetailViewUITest.swift
@@ -13,4 +13,13 @@ final class RepositoryDetailViewUITest: XCTestCase {
         app.terminate()
         super.tearDown()
     }
+    
+    /// 詳細画面にホームページセルと、READMEセルが表示されることを確認するテスト
+    func testShowCells() {
+        let repositoryDetailView = RepositorySearchPage(application: app).goToDetailView()
+        
+        _ = app.waitForExistence(timeout: 5)
+        XCTAssertTrue(repositoryDetailView.existsHomepageCell)
+        XCTAssertTrue(repositoryDetailView.existsReadmeCell)
+    }
 }
