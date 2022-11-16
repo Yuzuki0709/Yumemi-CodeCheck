9d70bb56d187ac5e4dd1b5f3aa64fe0ebafba623
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-16 18:56:43 +0900

    feat: RepositorySearchPageにIDsを追加

--- iOSEngineerCodeCheckUITests/PageObject/RepositorySearchPage.swift
+++ iOSEngineerCodeCheckUITests/PageObject/RepositorySearchPage.swift
@@ -1,6 +1,14 @@
 import XCTest
 
 final class RepositorySearchPage: PageObject {
+    
+    private enum IDs {
+        static let searchBar           = "search_searchresult_animationview"
+        static let serachButton        = "Search"
+        static let repositoryTableView = "search_repositorylist_tableview"
+        static let animationView       = "search_searchresult_animationview"
+    }
+    
     private let app: XCUIApplication
     
     required init(application: XCUIApplication) {
