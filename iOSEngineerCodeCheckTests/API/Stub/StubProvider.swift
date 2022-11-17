2fc85bc7e201510a6ce54f3b804d85aaf581e600
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-17 09:40:53 +0900

    feat: StubGitHubAPITargetProviderStatus404を追加

--- iOSEngineerCodeCheckTests/API/Stub/StubProvider.swift
+++ iOSEngineerCodeCheckTests/API/Stub/StubProvider.swift
@@ -17,3 +17,18 @@ struct StubGitHubAPITargetProviderStatus200: GitHubAPITargetProviderLoadable {
                                              stubClosure: MoyaProvider.immediatelyStub)
     }
 }
+
+struct StubGitHubAPITargetProviderStatus404: GitHubAPITargetProviderLoadable {
+    func load() -> MoyaProvider<GitHubAPITarget> {
+        let customEndpointClosure = { (target: GitHubAPITarget) -> Endpoint in
+            Endpoint(url: URL(target: target).absoluteString,
+                     sampleResponseClosure: { .networkResponse(404, Data())},
+                     method: target.method,
+                     task: target.task,
+                     httpHeaderFields: target.headers)
+        }
+        
+        return MoyaProvider<GitHubAPITarget>(endpointClosure: customEndpointClosure,
+                                             stubClosure: MoyaProvider.immediatelyStub)
+    }
+}
