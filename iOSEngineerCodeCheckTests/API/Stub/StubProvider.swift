5f0d07bc83a6c58d45ea1b5b01cfa2817ea69f4b
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-11-17 09:39:17 +0900

    feat: StubGitHubAPITargetProviderStatus200を追加

--- iOSEngineerCodeCheckTests/API/Stub/StubProvider.swift
+++ iOSEngineerCodeCheckTests/API/Stub/StubProvider.swift
@@ -2,3 +2,18 @@ import Foundation
 import Moya
 
 @testable import iOSEngineerCodeCheck
+
+struct StubGitHubAPITargetProviderStatus200: GitHubAPITargetProviderLoadable {
+    func load() -> MoyaProvider<GitHubAPITarget> {
+        let customEndpointClosure = { (target: GitHubAPITarget) -> Endpoint in
+            Endpoint(url: URL(target: target).absoluteString,
+                     sampleResponseClosure: { .networkResponse(200, target.sampleData)},
+                     method: target.method,
+                     task: target.task,
+                     httpHeaderFields: target.headers)
+        }
+        
+        return MoyaProvider<GitHubAPITarget>(endpointClosure: customEndpointClosure,
+                                             stubClosure: MoyaProvider.immediatelyStub)
+    }
+}
